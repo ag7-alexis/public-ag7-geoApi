f10867fa388486df72cf9109290a5ff322573e9e
ag7-alexis <59777795+ag7-alexis@users.noreply.github.com>
2020-10-06 10:19:32 +0200

    Update JwtProperties.java

--- src/main/java/ag7/dev/ag7geoapi/security/JwtProperties.java
+++ src/main/java/ag7/dev/ag7geoapi/security/JwtProperties.java
@@ -5,7 +5,7 @@ package ag7.dev.ag7geoapi.security;
  */
 
 public class JwtProperties {
-    public static final String SECRET = "AG7";
+    public static final String SECRET = "AG7"; // encryption key
     public static final String PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
 }
